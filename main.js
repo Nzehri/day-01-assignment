f476f9b50f0b3b4e50e3eecaa634edd547da8f8d
Nzehri <162792792+Nzehri@users.noreply.github.com>
2024-03-09 14:36:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+//let firstName = "rani";
+//console.log('hello' , firstName, "would you like to learn some TypeScript today?");
+//let firstName = "rani";
+//console.log(firstName.toLocaleLowerCase());
+//console.log(firstName.toUpperCase());
+//console.log(firstName.charAt(0).toUpperCase()+firstName.slice(1).toLowerCase());
+//console.log("rani\nRANI\nRani");
